b7091ba5f699085c3398fd68f4bbaf165dd13182
Mucyo Elie Joseph <147175412+mucyoelie@users.noreply.github.com>
2025-04-24 12:30:06 +0200

    PHP Assignment

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Elie Joseph PHP</title>
+</head>
+<body>
+    <h1>Elie Joseph PHP</h1>
+
+    <pre>
+    ***********
+    **       **
+    **
+    **
+    **
+    **       **
+    ***********
+    </pre>
+
+    <p>The SHA256 hash of "Elie Joseph" is:</p>
+    <p>
+        <?php
+            echo hash('sha256', 'Elie Joseph');
+        ?>
+    </p>
+
+    <p>
+        <a href="check.php">Click here to check the error setting</a><br>
+        <a href="fail.php">Click here to cause a traceback</a>
+    </p>
+</body>
+</html>
